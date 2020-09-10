7bd7a61fb8eea9dff80ee5e27d49f9984db69783
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-09-10 16:10:48 +0300

    fixed key missing in image upload api

--- swaptime_flutter/lib/consts/keys.dart
+++ swaptime_flutter/lib/consts/keys.dart
@@ -1,3 +1,3 @@
-class Keys {
+class ApiKeys {
   static const KEY_IMG_DB = '3f07b4d3683c49b7908aae838f6e47a6';
 }
--- swaptime_flutter/lib/module_upload/repository/upload_repository/upload_repository.dart
+++ swaptime_flutter/lib/module_upload/repository/upload_repository/upload_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:inject/inject.dart';
 import 'package:swaptime_flutter/api_urls.dart';
+import 'package:swaptime_flutter/consts/keys.dart';
 import 'package:swaptime_flutter/module_upload/response/imgbb/imgbb_response.dart';
 
 @provide
@@ -11,7 +12,11 @@ class UploadRepository {
       'image': await MultipartFile.fromFile(filePath),
     });
 
-    Response response = await client.post(ApiUrls.IMGBB, data: data);
+    Response response = await client.post(
+      ApiUrls.IMGBB,
+      data: data,
+      queryParameters: {'key': ApiKeys.KEY_IMG_DB},
+    );
     if (response == null) {
       return null;
     }
