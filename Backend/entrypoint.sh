91d6b11f21e6f2043943f6574078d4aa52e7bac9
AliOmoum <33194251+aliomom@users.noreply.github.com>
2020-11-04 20:44:04 +0200

    Update entrypoint.sh

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,6 +1,6 @@
 php bin/console make:migration
 php bin/console doctrine:migration:migrate
 apachectl -D FOREGROUND
-chmod -R 777 var/www/upload/image
+chmod -R 777 /var/www/upload/image
 chmod -R 777 /var/www/html/var
 
