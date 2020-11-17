ff5bcf96a3a67bc9e171fc381b4b237ae1a8c256
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-11-17 17:47:41 +0200

    added dropdown near name

--- swaptime_flutter/lib/games_module/ui/widget/game_card_list_header/game_card_list_header.dart
+++ swaptime_flutter/lib/games_module/ui/widget/game_card_list_header/game_card_list_header.dart
@@ -24,100 +24,90 @@ class _GameCardHeader extends State<GameCardListHeader> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        DropdownButton(
+          hint: Flex(
+            direction: Axis.horizontal,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.sort),
+              ),
+              Text(''),
+            ],
+          ),
+          onChanged: (by) {
+            widget.onSortChanged(by);
+          },
+          items: [
+            DropdownMenuItem(
+              value: SortByType.SORT_BY_ADDED,
+              child: Text(S.of(context).byAdded),
+            ),
+            DropdownMenuItem(
+              value: SortByType.SORT_BY_NAME,
+              child: Text(S.of(context).byName),
+            ),
+            DropdownMenuItem(
+              value: SortByType.SORT_BY_COMMENTS,
+              child: Text(S.of(context).byComments),
+            ),
+          ],
+        ),
         Text(S.of(context).gamesList),
         Flex(
-            direction: Axis.vertical,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Flex(
-                direction: Axis.horizontal,
-                children: [
-                  Container(
-                    color:
-                        widget.selectedCardType == GameCardType.GAME_CARD_LARGE
-                            ? SwapThemeDataService.getAccent()
-                            : SwapThemeDataService.getPrimary(),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        if (widget.selectedCardType !=
-                            GameCardType.GAME_CARD_LARGE) {
-                          widget.onTypeChanged(GameCardType.GAME_CARD_LARGE);
-                        }
-                      },
-                    ),
-                  ),
-                  Container(
-                    color:
-                        widget.selectedCardType == GameCardType.GAME_CARD_MEDIUM
-                            ? SwapThemeDataService.getAccent()
-                            : SwapThemeDataService.getPrimary(),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.apps,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        if (widget.selectedCardType !=
-                            GameCardType.GAME_CARD_MEDIUM) {
-                          widget.onTypeChanged(GameCardType.GAME_CARD_MEDIUM);
-                        }
-                      },
-                    ),
-                  ),
-                  Container(
-                    color:
-                        widget.selectedCardType == GameCardType.GAME_CARD_SMALL
-                            ? SwapThemeDataService.getAccent()
-                            : SwapThemeDataService.getPrimary(),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.grid_on,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        if (widget.selectedCardType !=
-                            GameCardType.GAME_CARD_SMALL) {
-                          widget.onTypeChanged(GameCardType.GAME_CARD_SMALL);
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              DropdownButton(
-                hint: Flex(
-                  direction: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(Icons.sort),
-                    ),
-                    Text('Sort By'),
-                  ],
+          direction: Axis.horizontal,
+          children: [
+            Container(
+              color: widget.selectedCardType == GameCardType.GAME_CARD_LARGE
+                  ? SwapThemeDataService.getAccent()
+                  : SwapThemeDataService.getPrimary(),
+              child: IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.white,
                 ),
-                onChanged: (by) {
-                  widget.onSortChanged(by);
+                onPressed: () {
+                  if (widget.selectedCardType != GameCardType.GAME_CARD_LARGE) {
+                    widget.onTypeChanged(GameCardType.GAME_CARD_LARGE);
+                  }
                 },
-                items: [
-                  DropdownMenuItem(
-                    value: SortByType.SORT_BY_ADDED,
-                    child: Text(S.of(context).byAdded),
-                  ),
-                  DropdownMenuItem(
-                    value: SortByType.SORT_BY_NAME,
-                    child: Text(S.of(context).byName),
-                  ),
-                  DropdownMenuItem(
-                    value: SortByType.SORT_BY_COMMENTS,
-                    child: Text(S.of(context).byComments),
-                  ),
-                ],
               ),
-            ]),
+            ),
+            Container(
+              color: widget.selectedCardType == GameCardType.GAME_CARD_MEDIUM
+                  ? SwapThemeDataService.getAccent()
+                  : SwapThemeDataService.getPrimary(),
+              child: IconButton(
+                icon: Icon(
+                  Icons.apps,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  if (widget.selectedCardType !=
+                      GameCardType.GAME_CARD_MEDIUM) {
+                    widget.onTypeChanged(GameCardType.GAME_CARD_MEDIUM);
+                  }
+                },
+              ),
+            ),
+            Container(
+              color: widget.selectedCardType == GameCardType.GAME_CARD_SMALL
+                  ? SwapThemeDataService.getAccent()
+                  : SwapThemeDataService.getPrimary(),
+              child: IconButton(
+                icon: Icon(
+                  Icons.grid_on,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  if (widget.selectedCardType != GameCardType.GAME_CARD_SMALL) {
+                    widget.onTypeChanged(GameCardType.GAME_CARD_SMALL);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
