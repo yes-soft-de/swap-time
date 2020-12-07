a46df7dbf042d7fa0281bb2a318543e59162ddda
ralsalloum <ramisalloum12@hotmail.com>
2020-12-07 20:44:38 +0200

    Delete Interaction was being added

--- Backend/src/Service/InteractionService.php
+++ Backend/src/Service/InteractionService.php
@@ -108,4 +108,9 @@ class InteractionService
             return $this->params;
         }
     }
+
+    public function deleteInteraction($swapItemID, $userID)
+    {
+        return $this->interactionManager->deleteInteraction($swapItemID, $userID);
+    }
 }
