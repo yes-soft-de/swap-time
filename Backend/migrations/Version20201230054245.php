45126069b507551a7e37e3a18077f298b8cb8892
Kenan <kenanhussein1@gmail.com>
2020-12-30 07:44:17 +0200

    fix migration

--- Backend/migrations/Version20201230054245.php
+++ Backend/migrations/Version20201230054245.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201229192354 extends AbstractMigration
+final class Version20201230054245 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -27,7 +27,7 @@ final class Version20201229192354 extends AbstractMigration
         $this->addSql('CREATE TABLE notification_token_entity (id INT AUTO_INCREMENT NOT NULL, user_id VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, date DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE report_entity (id INT AUTO_INCREMENT NOT NULL, user_id VARCHAR(255) NOT NULL, swap_item_id INT NOT NULL, date DATE NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE setting_entity (id INT AUTO_INCREMENT NOT NULL, upload_sub_folder VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE swap_entity (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, user_id_one VARCHAR(300) NOT NULL, user_id_two VARCHAR(300) NOT NULL, swap_item_id_one INT NOT NULL, swap_item_id_two INT NOT NULL, cost VARCHAR(12) DEFAULT NULL, room_id VARCHAR(300) DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE swap_entity (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, user_id_one VARCHAR(300) NOT NULL, user_id_two VARCHAR(300) NOT NULL, swap_item_id_one INT NOT NULL, swap_item_id_two INT NOT NULL, cost VARCHAR(12) DEFAULT NULL, room_id VARCHAR(300) DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, games_allowed_user_one JSON DEFAULT NULL, games_allowed_user_two JSON DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE swap_item_entity (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, tag JSON DEFAULT NULL, description LONGTEXT DEFAULT NULL, main_image VARCHAR(255) DEFAULT NULL, user_id VARCHAR(300) NOT NULL, platform VARCHAR(255) DEFAULT NULL, special_link TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_entity (id INT AUTO_INCREMENT NOT NULL, user_id VARCHAR(250) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) DEFAULT NULL, create_date DATE DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_6B7A5F55A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_profile_entity (id INT AUTO_INCREMENT NOT NULL, user_id VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, location VARCHAR(255) DEFAULT NULL, story LONGTEXT DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
